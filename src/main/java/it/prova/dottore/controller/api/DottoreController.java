b4175e05f3c561e3db1dc543c2573ca83ad49bc2
AlessandroCristiano <alessandrocristiano66@gmail.com>
2022-12-01 20:41:50 +0100

    fine implementazione dottore

--- src/main/java/it/prova/dottore/controller/api/DottoreController.java
+++ src/main/java/it/prova/dottore/controller/api/DottoreController.java
@@ -1,10 +1,55 @@
 package it.prova.dottore.controller.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.prova.dottore.dto.DottoreDTO;
+import it.prova.dottore.service.DottoreService;
 
 @RestController
 @RequestMapping("/api/dottore")
 public class DottoreController {
+
+	@Autowired
+	private DottoreService service;
+
+	@GetMapping
+	public List<DottoreDTO> listAll(){
+		return service.findAll().stream().map(dottore -> DottoreDTO.buildDottoreDTOFromModel(dottore)).collect(Collectors.toList());
+	}
+	
+	@GetMapping("/{id}")
+	public DottoreDTO findById(@PathVariable(name = "id", required = true) Long id) {
+		return DottoreDTO.buildDottoreDTOFromModel(service.findById(id));
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public DottoreDTO createNew(@RequestBody DottoreDTO input) {
+		return DottoreDTO.buildDottoreDTOFromModel(service.inserisciNuovo(input.buildDottoreModel()));
+	}
+	
+	@PutMapping
+	public DottoreDTO update (@RequestBody DottoreDTO input) {
+		return DottoreDTO.buildDottoreDTOFromModel(service.aggiorna(input.buildDottoreModel()));
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable(name = "id", required = true) Long id) {
+		service.rimuovi(id);
+	}	
 
 }
---
+++ src/main/java/it/prova/dottore/service/DottoreServiceImpl.java
@@ -0,0 +1,53 @@
+package it.prova.dottore.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.prova.dottore.model.Dottore;
+import it.prova.dottore.repository.DottoreRepository;
+
+@Service
+@Transactional
+public class DottoreServiceImpl implements DottoreService{
+	
+	@Autowired
+	private DottoreRepository repository;
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Dottore> findAll() {
+		return (List<Dottore>) repository.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Dottore findById(Long id) {
+		return repository.findById(null).orElse(null);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Dottore findByCodice(String codice) {
+		return repository.findByCodiceDottore(codice).stream().findFirst().orElse(null);
+	}
+
+	@Override
+	public Dottore inserisciNuovo(Dottore input) {
+		return repository.save(null);
+	}
+
+	@Override
+	public Dottore aggiorna(Dottore input) {
+		return repository.save(null);
+	}
+
+	@Override
+	public void rimuovi(Long id) {
+		repository.deleteById(id);
+		
+	}
+
+}
